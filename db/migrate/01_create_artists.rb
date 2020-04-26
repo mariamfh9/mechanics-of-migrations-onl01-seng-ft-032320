45d88c39092645ddc4e809bc980dff3fd0dff319
Mariam Haider <mfh.haider@gmail.com>
2020-04-26 20:42:01 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,11 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
+  
+  ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+
+
   def up
   end
  
