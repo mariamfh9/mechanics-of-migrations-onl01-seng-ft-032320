0ee1531a600cb51507184e4249347c919f8952f9
Mariam Haider <mfh.haider@gmail.com>
2020-04-26 20:43:41 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -23,7 +23,11 @@ ActiveRecord::Base.connection.execute(sql)
   end
   
   def change
-     create_table :artists do |t|
-  end
+      create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
   end
 end
