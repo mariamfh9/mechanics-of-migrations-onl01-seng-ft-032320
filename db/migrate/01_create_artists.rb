2bfdad4daa82e96713cd17cb5e35b174be7749c4
Mariam Haider <mfh.haider@gmail.com>
2020-04-26 20:51:42 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -2,19 +2,6 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   
  
  
-ActiveRecord::Base.connection.execute(sql)
-  def up
-  end
- 
-  def down
-  end
-  
-  def change
-    create_table :artists do |t|
-      t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
-    end
-  end
+class Artist < ActiveRecord::Base
+end
 end
