6fe0a4cfed0e8fa64a67df79353c28e6d007e37d
Mariam Haider <mfh.haider@gmail.com>
2020-04-26 20:51:57 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,7 +1,23 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
   
- 
- 
-class Artist < ActiveRecord::Base
-end
-end
+ cdef self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+      id INTEGER PRIMARY KEY, 
+      title TEXT, 
+      length INTEGER
+      )
+    SQL
+    ActiveRecord::Base.connection.execute(sql)
+  end
+
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+
+end 
